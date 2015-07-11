856ab605599346219f0dd0c6ea0cbcb905a3d6d6
zangzh13 <zangzh94@qq.com>
2015-07-12 00:15:21 +0800

    草稿

--- CONTROL.v
+++ CONTROL.v
@@ -1,7 +1,7 @@
 module control(
 	OpCode, funct, IRQ, 
 	PCSrc, RegDst, RegWr, 
-	ALUSrc1, ALUSrc2, ALUFun, Sign, 
+	ALUSrc1, ALUSrc2, ALUop, Sign, 
 	MemWr, MemRd, MemToReg,
 	EXTOp, LUOp
 	)
@@ -9,84 +9,147 @@ module control(
 	wire [5:0] OpCode;
 	wire [5:0] funct;
 	input IRQ;
-	output [2:0] PCSrc;
-	output [1:0] RegDst;
-	output RegWr, ALUSrc1, ALUSrc2, ALUFun, Sign, MemWr, MemRd;
-	output [1:0] MemToReg;
-	output EXTOp, LUOp;
+	
+	output reg [2:0] PCSrc;
+	output reg [1:0] RegDst;
+	output reg [2:0] ALUOp;
+	output reg [1:0] MemToReg;
+	output reg RegWr, ALUSrc2, ;
+	output ALUSrc1, Sign, MemWr, MemRd, EXTOp, LUOp;
 	
 	assign OpCode = instruct[31:26];
 	assign funct = instruct[5:0];
+	assign ALUSrc1 = (OpCode==0&&(funct==0||funct==6'h02||funct==6'h03))?1:0;
+	assign Sign = (OpCode==0&&(funct==6'h20||funct==6'h22)||OpCode==6'h08)?1:0;
+	assign MemWr = (OpCode==6'h2b)?1:0;
+	assign MemRd = (OpCode==6'h23)?1:0;
+	assign EXTOp = (OpCode==6'h09||OpCode==6'h0c||OpCode==6'h0b)?0:1;
+	assign LUOp = (OpCode==6'h0f)?1:0;
 	
 	always @(*)
-		case(OpCode)
-			//lw
-			6'h23:
-			//sw
-			6'h2b:
-			//lui
-			6'h2f:
-			//addi
-			6'h08:
-			//addiu
-			6'h09:
+		casex(OpCode)
+			//lw,sw,lui
+			6'b10_x011,6'b00_1111:
+				ALUOp <= 3'h0;
+				PCSrc <= (IRQ)?3'h4:3'h0;
+				RegDst <= (IRQ)?2'h3:2'h1;
+				RegWr <= (~OpCode[3])|OpCode[2];//lw:0x23,sw:0x2b,lui:0x0f
+				ALUSrc2 <= 1;
+				MemToReg <= (IRQ)?2:OpCode[5]; 
+			//addi,addiu
+			6'b00_100x:
+				ALUOp <= 3'h0;
+				PCSrc <= (IRQ)?3'h4:3'h0;
+				RegDst <= (IRQ)?2'h3:2'h1;
+				RegWr <= (IRQ)?1:0;
+				ALUSrc2 <= 1;
+				MemToReg <= (IRQ)?2:0;
 			//andi
-			6'h0c
-			//slti
-			6'h0a:
-			//sltiu
-			6'h0b:
-			//beq
-			6'h04:
-			//bne
-			6'h05:
-			//blez
-			6'h06:
-			//bgtz
-			6'h07:
-			//bgez
-			6'h01:
+			6'b00_1100:
+				ALUOp <= 3'h0;
+				PCSrc <= (IRQ)?3'h4:3'h0;
+				RegDst <= (IRQ)?2'h3:2'h1;
+				RegWr <= (IRQ)?1:0;
+				ALUSrc2 <= 1;
+				MemToReg <= (IRQ)?2:0;
+			//slti,sltiu
+			6'b00_101x:
+				ALUOp <= 3'h3;
+				PCSrc <= (IRQ)?3'h4:3'h0;
+				RegDst <= (IRQ)?2'h3:2'h1;
+				RegWr <= (IRQ)?1:0;
+				ALUSrc2 <= 1;
+				MemToReg <= (IRQ)?2:0;
+			//beq,bne,blez,bgtz,bgez
+			6'b00_01xx, 6'b00_0001:
+				ALUOp <= OpCode[2:0];
+				PCSrc <= (IRQ)?3'h4:3'h1;
+				RegDst <= (IRQ)?2'h3:2'h0;
+				RegWr <= (IRQ)?1:0;
+				ALUSrc2 <= 0;
+				MemToReg <= (IRQ)?2:0;
 			//j
 			6'h02:
+				ALUOp <= 3'h0;
+				PCSrc <= (IRQ)?3'h4:3'h2;
+				RegDst <= (IRQ)?2'h3:2'h2;
+				RegWr <= (IRQ)?1:0;
+				ALUSrc2 <= 1;
+				MemToReg <= 2;
 			//jal
 			6'h03:
-			//
+				ALUOp <= 3'h0;
+				PCSrc <= (IRQ)?3'h4:3'h2;
+				RegDst <= (IRQ)?2'h3:2'h2;
+				RegWr <= 1;
+				ALUSrc2 <= 1;
+				MemToReg <= 2;
+			//R-type
 			6'h0:
-				case(funct)
-					//add
-					6'h20:
-					//addu
-					6'h21:
-					//sub
-					6'h22:
-					//subu
-					6'h23:
-					//and
-					6'h24:
-					//or
-					6'h25:
-					//xor
-					6'h26:
-					//nor
-					6'h27:
-					//sll，nop
-					6'h00:
-					//srl
-					6'h02:
-					//sra
-					6'h03:
-					//slt
-					6'h2a:
-					//sltu
-					6'h2b:
+				casex(funct)
+					6'b00_0000,6'b00_001x,6'b00_100x,6'b10_0xxx,6'b10_x01x:
+						ALUOp <= 3'h2;
+						PCSrc <= (IRQ)?3'h4:3'h0;
+						RegDst <= (IRQ)?2'h3:2'h0;
+						RegWr <= 1;
+						ALUSrc2 <= 0;
+						MemToReg <= (IRQ)?2:0;
 					//jr
 					6'h08:
+						ALUOp <= 3'h0;
+						PCSrc <= (IRQ)?3'h4:3'h3;
+						RegDst <= (IRQ)?2'h3:2'h2;
+						RegWr <= (IRQ)?1:0;
+						ALUSrc2 <= 1;
+						MemToReg <= (IRQ)?2:0;
 					//jalr
 					6'h09:
-	assign PCSrc = (OpCode == 6'h4 || OpCode == 6'h5 || OpCode == 6'h6 || OpCode == 6'h7 || OpCode == 6'h1)?1:
-					(OpCode ==6'h2 || OpCode == 6'h3)?2:
-					(OpCode == 6'h23 || OpCode == 6'h2b || OpCode == 6'hf || OpCode == 6'h8 ||
-						OpCode == 6'h9 || OpCode == 6'hc || OpCode == 6'ha || OpCode == 6'hb ||)?0:
-					(OpCode == 0)?
-						(funct == 0 || funct == 6'h20 || funct == 6'h21 || funct == 6'h22 ||
-							funct == 6'h23 || funct == 6'h24 ||)
+						ALUOp <= 3'h0;
+						PCSrc <= (IRQ)?3'h4:3'h3;
+						RegDst <= (IRQ)?2'h3:2'h2;
+						RegWr <= 1;
+						ALUSrc2 <= 1;
+						MemToReg <= (IRQ)?2:0;
+					//funct undefined
+					default:
+						ALUOp <= 3'h0;
+						PCSrc <= 3'h5;
+						RegDst <= 2'h3;
+						RegWr <= 1;
+						ALUSrc2 <= 1;
+						MemToReg <= (IRQ)?2:0;
+				endcase
+			//OpCode undefined
+			default:
+		endcase
+endmodule
+
+module ALUCtl(ALUOp, funct, ALUFun)
+	input [2:0] ALUOp;
+	input [5:0] funct;
+	output reg [5:0] ALUFun;
+	always @(*)
+		case(ALUOp)
+			3'h0:ALUFun<=6'h00;
+			3'h2:
+				casex(funct)
+					6'b10_000x:ALUFun<=6'h00;
+					6'b10_001x:ALUFun<=6'h01;
+					6'h24:ALUFun<=6'h18;
+					6'h25:ALUFun<=6'h1e;
+					6'h26:ALUFun<=6'h16;
+					6'h27:ALUFun<=6'h11;
+					6'h00:ALUFun<=6'h20;
+					6'h02:ALUFun<=6'h21;
+					6'h03:ALUFun<=6'h23;
+					6'b10_101x:ALUFun<=6'h35;
+					default:ALUFun<=6'h00;
+				endcase
+			3'h3:ALUFun<=6'h35;
+			3'h4:ALUFun<=6'h33;
+			3'h5:ALUFun<=6'h31;
+			3'h6:ALUFun<=6'h3d;
+			3'h7:ALUFun<=6'h3f;
+			3'h1:ALUFun<=6'h39;
+		endcase
+endmodule
